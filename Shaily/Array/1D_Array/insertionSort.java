d78c41db4da8e44a72b4a5f65ede94854720f6c3
Yashendra Singh <110024255+yash8931@users.noreply.github.com>
2024-10-23 22:10:41 +0530

    Update insertionSort.java

--- Shaily/Array/1D_Array/insertionSort.java
+++ Shaily/Array/1D_Array/insertionSort.java
@@ -10,8 +10,19 @@ public class insertionSort {
             // Move elements of arr[0..i-1], that are greater than key,
             // to one position ahead of their current position
 
-            // 12, 11, 13, 5, 6   i = 1  j = 0  key = 11, arr[j] = 12
-            // 12, 12, 13
+            // 12, 11, 13, 5, 6       i = 1  j = 0  key = 11, arr[j] = 12
+            // 12, 12, 13, 5, 6,      j = -1 
+            // 11, 12, 13, 5, 6       i = 2 j = 1 key = 13 arr[j] = 12
+            // 11, 12, 13, 5, 6       i = 3 j = 2 key = 5 arr[j] = 13
+            // 11, 12, 13, 13, 6      i = 3 j = 1 key = 5 arr[j] = 12
+            // 11, 12, 12, 13, 6      i = 3 j = 0 key = 5 arr[j] = 11
+            // 11, 11, 12, 13, 6      i = 3 j = -1 key = 5 loop break
+            // arr[0] = 5
+            // 5, 11, 12, 13, 6       i = 4 j = 3 key = 6 arr[j] = 13
+            // 5, 11, 12, 13, 13      i = 4 j = 2 key = 6 arr[j] = 12
+            // 5, 11, 12, 12, 13      i = 4 j = 1 key = 6 arr[j] = 11
+            // 5, 11, 11, 12, 13      i = 4 j = 0 key = 6 arr[j] = 5  loop break
+            // 5, 6, 11, 12, 13         sorted array
             
 
             while (j >= 0 && arr[j] > key) {
